a4941643603244afe1b982010e77cd7139acb367
Luis A. Mesa <luisguzman014.m@gmail.com>
2022-12-27 23:13:52 -0500

    # Se crea SplashActivity.kt - Se crea el Splash para la introducción a la app, y el acuerdo de las politicas de privacidad requerido por Google.

---
+++ app/src/main/java/com/lamesa/socialdown/view/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.lamesa.socialdown.view.splash
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.lamesa.socialdown.databinding.ActivitySplashBinding
+import com.lamesa.socialdown.utils.DialogXUtils
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Comprobrar que el usuario acepte las politicas de privacidad
+        Handler().postDelayed({
+            DialogXUtils.Policies.dialogPrivacyPolicies(this)
+        }, 1500)
+
+    }
+
+}
